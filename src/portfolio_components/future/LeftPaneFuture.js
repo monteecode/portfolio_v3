dcf98d3b13165ea0551822d99f20a06891179684
Montensia Banks <montensia@gmail.com>
2022-03-30 18:21:54 -0500

    Added image and text for nutrimeasure and text for timekeeper

--- src/portfolio_components/future/LeftPaneFuture.js
+++ src/portfolio_components/future/LeftPaneFuture.js
@@ -4,7 +4,29 @@ const LeftPaneCK = () => {
   return (
     <div className="leftPane">
       <h1>Projects In Current Development</h1>
-      <p>I'm text for Current Projects in Development</p>
+      <h2>Timekeeper</h2>
+      <p>Allows a user to enter and calculate their time per day in a month</p>
+      <h3>Technolgies</h3>
+      <ul>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Ruby on Rails</li>
+        <li>Postgres</li>
+      </ul>
+
+      <h2>Nutrimeasure</h2>
+      <p>
+        A user can look up how much nutrients are in a particular food item.
+        This allows the user to understand how much nutrients one can take
+        naturally from food to increase their nutrient levels in their bodies.
+      </p>
+      <h3>Technolgies</h3>
+      <ul>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Node</li>
+        <li>Postgres</li>
+      </ul>
     </div>
   );
 };
--- src/portfolio_components/future/RightPaneFuture.js
+++ src/portfolio_components/future/RightPaneFuture.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
+import timekeeper from "../../img/portfolio/timekeeper_729x450.jpg";
+import nutrimeasure from "../../img/portfolio/nutrimeasure_722x450.jpg";
 
-const RightPaneCK= () => {
-    return (
-        <div className="rightPane">
-            <div>
-                <img src="https://www.common.com/images/studio/services/common-melrose-at-twilight.jpg" />
-            </div>
-            <div>
-                <img src="https://www.common.com/images/studio/services/dining-room-at-common-melrose.jpg" />
-            </div>
-        </div>
-    )
-}
+const RightPaneCK = () => {
+  return (
+    <div className="rightPane">
+      <div>
+        <h4>Timekeeper</h4>
+        <img src={timekeeper} alt="Calendar from Timekeeper app" />
+      </div>
+      <div>
+        <h4>Nutrimeasure</h4>
+        <img src={nutrimeasure} alt="Nutrimeasure app home page" />
+      </div>
+    </div>
+  );
+};
 
-export default RightPaneCK
+export default RightPaneCK;
