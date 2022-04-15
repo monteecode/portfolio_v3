381800757a956dc74842a8323f463e32354030ac
Montensia Banks <montensia@gmail.com>
2022-04-15 14:17:11 -0500

    Deleted unnecessary comments

--- src/portfolio_components/timekeeper/RightPaneImg.js
+++ src/portfolio_components/timekeeper/RightPaneImg.js
@@ -8,49 +8,12 @@ import timekeeperDayTime from "../../img/portfolio/timekeeper_day_time_889x500.j
 import timekeeperAddTime from "../../img/portfolio/timekeeper_add_time_769x500.jpg";
 
 const RightPaneImg = () => {
-  console.log(timekeeperDays);
   return (
     <div className="rightPane">
       <ZoomImage timeKeeperImg={timekeeperHome} />
       <ZoomImage timeKeeperImg={timekeeperDays} />
       <ZoomImage timeKeeperImg={timekeeperDayTime} />
       <ZoomImage timeKeeperImg={timekeeperAddTime} />
-      {/* <TransformWrapper
-        defaultScale={1}
-        defaultPositionX={1}
-        defaultPositionY={1}
-      >
-        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-          <div className="imgWrapper">
-            <TransformComponent>
-              <img src={timekeeperHome} alt="Timekeeper app home page" />
-            </TransformComponent>
-            <div className="col-lg-12 text-center mb-3">
-              <button
-                className="btn btn-outline-primary mr-2"
-                onClick={() => zoomIn()}
-              >
-                + Zoom In
-              </button>
-              <button
-                className="btn btn-outline-primary mr-2"
-                onClick={() => zoomOut()}
-              >
-                - Zoom Out
-              </button>
-            </div>
-          </div>
-        )}
-      </TransformWrapper> */}
-      {/* <div>
-        <img src={timekeeperDays} alt="Atlanta and Atlanta Information" />
-      </div>
-      <div>
-        <img src={timekeeperDayTime} alt="Atlanta data on essential costs" />
-      </div>
-      <div>
-        <img src={timekeeperAddTime} alt="Atlanta data on weather" />
-      </div> */}
     </div>
   );
 };
