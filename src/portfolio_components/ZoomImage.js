66c7288a1cb620024e9d2d06177a219cc1b7ecb6
Montensia Banks <montensia@gmail.com>
2022-04-14 21:26:47 -0500

    Added zoom options to one image and started with creation of component for zoom images

---
+++ src/portfolio_components/ZoomImage.js
@@ -0,0 +1,19 @@
+// import React from "react";
+// import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+// //import timekeeperHome from "../../img/portfolio/timekeeper_home.jpg";
+
+// const ZoomImage = () => {
+//   return (
+//     <TransformWrapper
+//       defaultScale={1}
+//       defaultPositionX={100}
+//       defaultPositionY={100}
+//     >
+//       {/* <TransformComponent>
+//         <img src={timekeeperHome} immageProp={} alt="Timekeeper app home page" />
+//       </TransformComponent> */}
+//     </TransformWrapper>
+//   );
+// };
+
+// export default ZoomImage;
--- src/portfolio_components/timekeeper/RightPaneImg.js
+++ src/portfolio_components/timekeeper/RightPaneImg.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import ZoomImage from "../ZoomImage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import timekeeperHome from "../../img/portfolio/timekeeper_home.jpg";
 import timekeeperDays from "../../img/portfolio/timekeeper_days_868x500.jpg";
 import timekeeperDayTime from "../../img/portfolio/timekeeper_day_time_889x500.jpg";
@@ -8,14 +10,33 @@ import timekeeperAddTime from "../../img/portfolio/timekeeper_add_time_769x500.j
 const RightPaneImg = () => {
   return (
     <div className="rightPane">
+      {/* <ZoomImage /> */}
       <TransformWrapper
         defaultScale={1}
-        defaultPositionX={100}
-        defaultPositionY={100}
+        defaultPositionX={1}
+        defaultPositionY={1}
       >
-        <TransformComponent>
-          <img src={timekeeperHome} alt="Timekeeper app home page" />
-        </TransformComponent>
+        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+          <div className="imgWrapper">
+            <TransformComponent>
+              <img src={timekeeperHome} alt="Timekeeper app home page" />
+            </TransformComponent>
+            <div className="col-lg-12 text-center mb-3">
+              <button
+                className="btn btn-outline-primary mr-2"
+                onClick={() => zoomIn()}
+              >
+                + Zoom In
+              </button>
+              <button
+                className="btn btn-outline-primary mr-2"
+                onClick={() => zoomOut()}
+              >
+                - Zoom Out
+              </button>
+            </div>
+          </div>
+        )}
       </TransformWrapper>
       <div>
         <img src={timekeeperDays} alt="Atlanta and Atlanta Information" />
